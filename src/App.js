b46c7890ec76451ccc5de5af5b05ae55043b1a0e
Zedkaere <zakariaaslimani1080@gmail.com>
2023-02-16 09:39:43 +0100

    code source

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import desert from './desert.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Le premier</h1>
+      <img 
+      src={desert} 
+      alt="Le desert" 
+      width={600} />
+    </div>
+  );
+}
+
+export default App;
